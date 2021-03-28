247d48a56afbebe3d91739c174466bcfda04b860
Michal Mišo Červeňák <miso@fykos.cz>
2021-03-28 05:40:32 +0200

    Add ginature request

--- tools/index.php
+++ tools/index.php
@@ -1,12 +1,7 @@
 <?php
 
 use Fykosak\FKSDBDownloaderCore\FKSDBDownloader;
-use Fykosak\FKSDBDownloaderCore\Requests\{
-    Event\ParticipantsListRequest,
-    Event\TeamsListRequest,
-    EventListRequest,
-    ExportRequest
-};
+use Fykosak\FKSDBDownloaderCore\Requests\{Event\ParticipantsListRequest, Event\TeamsListRequest, EventListRequest, ExportRequest, SignaturesRequest};
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/pass.php';
@@ -16,7 +11,9 @@ $downloader = new FKSDBDownloader(FKSDB_WSDL, FKSDB_USER, FKSDB_PASS);
 //$result = $downloader->download(new TeamsListRequest(145));
 //$result = $downloader->download(new ParticipantsListRequest(145));
 //$result = $downloader->download(new EventListRequest([1, 9]));
-$result = $downloader->download(new ExportRequest('orgs.ex', ['contest' => 'fykos']));
+$result = $downloader->download(new SignaturesRequest(2));
+
+//$result = $downloader->download(new ExportRequest('orgs.ex', ['contest' => 'fykos']));
 
 header('Content-Type: text/xml');
 echo $result;
